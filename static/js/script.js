26eecddba040d04cfa68e9c44b032908517b5c2a
OpenGlobe <openglobe@gmail.com>
2015-03-19 15:07:34 -0400

    Added countdown...

---
+++ static/js/script.js
@@ -0,0 +1,37 @@
+$(function(){
+	
+	var note = $('#note'),
+        // Countdown to April 11, 2015
+		ts = new Date(2015, 3, 11, 7),
+		newYear = false;
+	
+	if((new Date()) > ts){
+		// The new year is here! Count towards something else.
+		// Notice the *1000 at the end - time must be in milliseconds
+		ts = (new Date()).getTime() + 10*24*60*60*1000;
+		newYear = false;
+	}
+		
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+			message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+			
+			if(newYear){
+				message += "left until the new year!";
+			}
+			else {
+				message += "left until the myAmerica Developer Summit!";
+			}
+			
+			note.html(message);
+		}
+	});
+	
+});
